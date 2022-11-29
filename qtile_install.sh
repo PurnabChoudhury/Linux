2ce58fd44d346822ca482d863ee2a6a5d7c4e8a0
PurnabChoudhury <118185031+PurnabChoudhury@users.noreply.github.com>
2022-11-29 20:24:48 +0530

    Update qtile_install.sh

--- qtile_install.sh
+++ qtile_install.sh
@@ -1,1 +1,7 @@
-
+apt install python3-pip -y
+pip3 install xcffib
+pip3 install qtile
+pip3 install psutil
+apt install nitrogen pcmanfm lxappearance kvantum libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev -y
+cd /usr/share/xsessions
+touch qtile.desktop
